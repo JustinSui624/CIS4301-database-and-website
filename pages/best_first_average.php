53dff7dd8548faef6deb9348985813e8d4002ea1
JustinSui624 <139939511+JustinSui624@users.noreply.github.com>
2024-12-04 08:02:27 -0500

    Update best_first_average.php

--- pages/best_first_average.php
+++ pages/best_first_average.php
@@ -3,6 +3,26 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php print( "Hello World"); ?>
+    <?php 
+    include_once 'connect.php';
+    
+$sql = "SELECT * FROM `person` WHERE person_id = " .$user_id." ";
+ $conn = oci_connect($username = 'j.sui',
+                          $password = 'Q38vOyjmt1DQngytzjMzmMnN',
+                          $connection_string = '//oracle.cise.ufl.edu/orcl');
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+    echo "user_id: " . $row["person_id"]. " - person_id: " . $row["person_first"]. " " . $row["person_last"]. "<br>";
+    $person_id = $row["person_id"];
+    $person_first = $row["person_first"];
+}
+} else {
+echo $sql;
+}
+$conn->close();
+
+?>
   </body>
 </html>
